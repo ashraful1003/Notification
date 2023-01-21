5aeee5a04f92f989af042f6002c43b9cf0220684
Ashraful Alam <alamashraful1003@gmail.com>
2023-01-21 12:52:38 +0600

    some updates

--- app/src/main/java/com/examples/notifications/MainActivity.java
+++ app/src/main/java/com/examples/notifications/MainActivity.java
@@ -23,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     /// initialize the buttons
     Button toastBtn, customToastBtn, progressBtn;
 
+    /// for progress notification
+    String CHANNEL_ID = "Notification";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,12 +41,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 /// make the toast
-                Toast toast = Toast.makeText(MainActivity.this, "Simple Toast", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(MainActivity.this, "Sample Toast", Toast.LENGTH_SHORT);
                 /// you can set the gravity to where you want to show the toast.
                 //
                 /// if there's necessary comment out use otherwise remove from the line setGravity only
                 //toast.setGravity(Gravity.TOP | Gravity.RIGHT, 100, 250);
-
                 toast.show();
             }
         });
@@ -52,11 +54,14 @@ public class MainActivity extends AppCompatActivity {
         customToastBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /// inflate the layout
                 LayoutInflater inflater = getLayoutInflater();
                 View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.custom_toast_layout));
                 TextView textView = layout.findViewById(R.id.text_view);
-                textView.setText("Custom Toast Notification");
-                Toast toast = new Toast(getApplicationContext());
+                textView.setText("Custom Toast");
+
+                /// toasting
+                Toast toast = new Toast(MainActivity.this);
                 toast.setGravity(Gravity.BOTTOM, 0, 100);
                 toast.setDuration(Toast.LENGTH_LONG);
                 toast.setView(layout);
@@ -74,15 +79,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addNotification() {
+        /// for permission purpose
         createNotificationChannel();
+
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this, "here")
+                new NotificationCompat.Builder(this, CHANNEL_ID)
                         .setSmallIcon(R.drawable.icons)
                         .setContentTitle("Progress Notifications")
                         .setContentText("This is a progress notification")
                         .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        /// this is thread used to run task on background automatically for pre-defined time
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -101,6 +109,10 @@ public class MainActivity extends AppCompatActivity {
                 manager.notify(0, builder.build());
             }
         }).start();
+
+        /// if we are given progress notification rather only notification,
+        // just comment the full thread and remove comment from the below line
+//        manager.notify(0, builder.build());
     }
 
     /// for permission
@@ -109,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("here", name, importance);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
